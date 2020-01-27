a76b307c5e445953344f52f3fb6b6b36aa8c9e54
fangyang_gpu <fangyang.jing@hotmail.com>
2020-01-27 19:19:05 +0800

    [Bug fixed] sqlalchemy驱动对操作系统兼容性问题 1. 实例化时根据操作系统不同, 生成不同的engine

--- vnpy/app/cta_backtester_jnpy/db_operation.py
+++ vnpy/app/cta_backtester_jnpy/db_operation.py
@@ -10,13 +10,23 @@ import pandas as pd
 from sqlalchemy import create_engine
 from vnpy.trader.setting import get_settings
 from vnpy.trader.utility import get_file_path
+import platform
 
 
 class DBOperation:
     def __init__(self, settings_dict):
         self.settings_dict = settings_dict
         self.file_path_str = get_file_path(settings_dict['database'])
-        self.engine = create_engine(f"{self.settings_dict['driver']}:////{self.file_path_str}")
+
+        os_str = platform.system()
+        if os_str == "Windows":
+            sqlite_os = "/"
+        elif os_str == "Linux":
+            sqlite_os = "//"
+        else:
+            print(f"OS is {os_str}. DBoperation may meet problem.")
+
+        self.engine = create_engine(f"{self.settings_dict['driver']}://{sqlite_os}{self.file_path_str}")
 
     def get_groupby_data_from_sql_db(self):
         sql = "select exchange, symbol, interval, count(1) from dbbardata group by symbol, interval, exchange;"
