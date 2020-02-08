42a3c365404845e12bf1d5d630223f6b4f75ad10
fangyang <fangyang.jing@hotmail.com>
2020-02-08 15:59:19 +0800

    [Update] 更新强化学习环境代码

--- vnpy/app/cta_backtester_jnpy/DRL/environ_future.py
+++ vnpy/app/cta_backtester_jnpy/DRL/environ_future.py
@@ -23,7 +23,7 @@ class FutureEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(
-            self, prices_df: pd.DataFrame, action_ratio_dict: dict, bars_count: int = DEFAULT_BARS_COUNT,
+            self, prices_df: pd.DataFrame, bars_count: int = DEFAULT_BARS_COUNT,
             commission_rate: float = DEFAULT_COMMISSION_RATE, time_cost: float = DEFAULT_TIME_COST,
             balance: float = BALANCE, security_rate: float = DEFAULT_SECURITY_RATE,
             contract_prod: int = DEFAULT_CONTRACT_PROD
@@ -36,13 +36,16 @@ class FutureEnv(gym.Env):
         assert balance >= 0.0, "Balance should >= 0.0"
         assert len(prices_df) > 0, "DataFrame length should > 0"
 
-        self.action_ratio_dict = action_ratio_dict
         self.ix = 0  # 每次游戏开始数据的起始的索引
         self.step_len = bars_count  # 每个step的长度
         self.step_n = 1  # 连续obs 相邻 steps 之间的跨度, 为了简化计算, 这里默认为1步, 如果想改周期, 请修改输入的KBar周期
         self.have_long_position = False  # 是否持多仓
         self.have_short_position = False  # 是否持空仓
         self.new_df_first_bar_open_price = 0.0  # 开仓价格
+        self.buy_condition = False
+        self.sell_condition = False
+        self.short_condition = False
+        self.cover_condition = False
 
         self.commission_rate = commission_rate  # 手续费率
         self.contract_prod = contract_prod  # 合约乘数
@@ -52,9 +55,9 @@ class FutureEnv(gym.Env):
         self.start_balance = balance
         self.hold_money_value = self.start_balance
         self.hold_share_value = 0.0
-        self.net_position = 0
-        self.long_position = 0
-        self.short_position = 0
+        self.net_position = 0  # 带正负号
+        self.long_position = 0  # 不带正负号
+        self.short_position = 0  # 不带正负号
         self.long_profit_df = pd.DataFrame()
         self.short_profit_df = pd.DataFrame()
 
@@ -73,12 +76,7 @@ class FutureEnv(gym.Env):
         self.cur_state_df = pd.DataFrame()
         self.trade_record_df = pd.DataFrame()
 
-        self.action_space = gym.spaces.Tuple(
-            spaces=(
-                gym.spaces.Discrete(n=len(self.action_ratio_dict)),
-                gym.spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32)
-            )
-        )
+        self.action_space = gym.spaces.Box(low=-1.1, high=1.1, shape=(1,), dtype=np.float32)
         # shape 为 df columns (包含时间, 去掉日期) + position + reward
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf,
                                                 shape=(self.prices_df.shape[1],),
@@ -98,11 +96,11 @@ class FutureEnv(gym.Env):
         #     pass
 
         # self.total_position = start_position
-        # self.offset_total_price = self.cur_state_df['<OPEN>'][start_position_index] * self.total_position
+        # self.offset_total_price = self.cur_state_df['open_price'][start_position_index] * self.total_position
 
         return self.cur_state_df
 
-    def update_new_df(self, action:str, new_df: pd.DataFrame, commission: float, trade_num: int):
+    def update_new_df(self, action: float, new_df: pd.DataFrame, commission: float, trade_num: int):
         """
         记录 开平仓价格 和 位置,
         给出step_reward均摊更新到new step的每个bar上
@@ -116,10 +114,11 @@ class FutureEnv(gym.Env):
                                               * self.new_df_first_bar_open_price \
                                               * self.security_rate
         # 花掉这么多价值的钱+手续费
-        if action == "Buy" or action == "Sell":
+        if self.buy_condition or self.sell_condition:
             self.hold_money_value -= (trade_share_on_first_bar_open_value + commission)
-        elif action == "Short" or action == "Cover":
+        elif self.short_condition or self.cover_condition:
             self.hold_money_value += (trade_share_on_first_bar_open_value - commission)
+
         # 账户余额
         new_df['hold_money_value'] = self.hold_money_value
 
@@ -129,42 +128,55 @@ class FutureEnv(gym.Env):
         self.trade_record_df = self.trade_record_df.append(new_df.iloc[0])
 
         # 实时净值
-        if action == "Buy" or action == "Sell":
+        if self.buy_condition or self.sell_condition:
             # 持仓share的实时价值
             new_df['hold_share_value'] = new_df.apply(
-                lambda x: x['<CLOSE>'] * self.contract_prod * self.security_rate * x['net_position'], axis=1)
+                lambda x: x['close_price'] * self.contract_prod * self.security_rate * x['long_position'], axis=1)
             new_df['balance'] = new_df['hold_share_value'] + self.hold_money_value
             new_df['profit'] = new_df['balance'] - self.start_balance
 
-            self.have_long_position = True if self.long_position > 0 else False
-
-        elif action == "Short" or action == "Cover":
+        elif self.short_condition or self.cover_condition:
             new_df['hold_share_value'] = new_df.apply(
-                lambda x: x['<CLOSE>'] * self.contract_prod * self.security_rate * abs(x['net_position']), axis=1)
-            new_df['balance'] = 2 * (self.start_balance - commission) - abs(new_df['hold_share_value']) - self.hold_money_value
+                lambda x: x['close_price'] * self.contract_prod * self.security_rate * abs(x['short_position']), axis=1)
+            new_df['balance'] = 2 * (self.start_balance - commission) - abs(
+                new_df['hold_share_value']) - self.hold_money_value
             new_df['profit'] = new_df['balance'] - self.start_balance
-            self.have_short_position = True if self.short_position < 0 else False
+
+        # 在上面条件判断后, 更新多空持仓逻辑
+        self.have_long_position = True if self.long_position > 0 else False
+        self.have_short_position = True if self.short_position > 0 else False
 
         return new_df, new_df['balance'].iloc[-1] - self.start_balance
 
-    def get_cur_state_trade_num(self, action, current_price):
+    def get_cur_state_trade_num_by_money(self, action: float, current_price: float):
         """通过多空仓位资金使用比例, 计算当前状态下最后一根bar收盘价可交易的数量"""
         # (买入比例 * 账户金额) / (当前价格 * 合约乘数 * 保证金比例) = 交易手数
         return int(
-            (self.action_ratio_dict[action][0] * self.hold_money_value) / (
-                        current_price * self.contract_prod * self.security_rate))
+            (action * self.hold_money_value) / (
+                    current_price * self.contract_prod * self.security_rate))
+
+    def get_cur_state_trade_num_by_share(self, action: float, current_price: float):
+        """通过持仓可使用比例, 计算当前状态下最后一根bar收盘价可交易的数量"""
+        # (买入比例 * 账户金额) / (当前价格 * 合约乘数 * 保证金比例) = 交易手数
+        return int(action * abs(self.net_position))
 
     def get_next_state_commission(self, trade_num):
         """ float: = 交易手数 * 合约乘数 * 成交价格(open) * 交易费率 """
-        return trade_num * self.contract_prod * self.new_df_first_bar_open_price * self.commission_rate
+        return abs(trade_num) * self.contract_prod * self.new_df_first_bar_open_price * self.commission_rate
 
-    def step(self, action):
+    def step(self, action: float):
         # 给出下一个状态
         # obs中含有全局reward, Agent设计的时候需要进行记忆
         obs = self.cur_state_df.iloc[self.step_n:, :]
         obs_last_index = self.cur_state_df.index.to_list()[-1]
         reward = 0
         done = False
+
+        # 更新action条件
+        self.buy_condition = action > 0.0 and not self.have_short_position
+        self.sell_condition = action < 0.0 and self.have_long_position
+        self.short_condition = action < 0.0 and not self.have_long_position
+        self.cover_condition = action > 0.0 and self.have_short_position
 
         if obs_last_index + 1 + self.step_n > self.prices_df_length:
             done = True
@@ -173,34 +185,35 @@ class FutureEnv(gym.Env):
 
         new_df = self.prices_df.iloc[obs_last_index + 1: obs_last_index + 1 + self.step_n]
         # 获取当前状态的最后收盘价, 用于计算开仓数量
-        current_price = obs['<CLOSE>'].iloc[-1]
-        trade_num = self.get_cur_state_trade_num(action, current_price)
-        self.new_df_first_bar_open_price = new_df['<OPEN>'].iloc[0]
-
-        commission = self.get_next_state_commission(trade_num)
+        current_price = obs['close_price'].iloc[-1]
+        # buy, sell 通过hold_money来计算比例
+        if self.buy_condition or self.short_condition:
+            trade_num = self.get_cur_state_trade_num_by_money(action, current_price)  # 有正负, 和action一致
+        else:
+            trade_num = self.get_cur_state_trade_num_by_share(action, current_price)
+        self.new_df_first_bar_open_price = new_df['open_price'].iloc[0]
+        commission = self.get_next_state_commission(trade_num)  # no sign >= 0
 
         # 给出下一个状态的 reward 和 done flag
         # TODO 这里环境预设不会双向持仓, 保证训练出来的模型不会有相同行为
-        # 开仓
-        if action == "Buy" and not self.have_short_position:
-            trade_num = trade_num
+        # 开仓 Buy
+        if self.buy_condition:
             self.long_position += trade_num
             new_df, reward = self.update_new_df(action, new_df, commission, trade_num)
-
-        elif action == "Sell" and self.have_long_position:
+        # 平仓 sell
+        elif self.sell_condition:
             # 减持多仓数目不能大于持仓量, 大于的部分按照全平处理
-            trade_num, done = (-trade_num, False) if trade_num < self.long_position else (-self.long_position, True)
+            trade_num, done = (trade_num, False) if abs(trade_num) < self.long_position else (-self.long_position, True)
             self.long_position += trade_num
             new_df, reward = self.update_new_df(action, new_df, commission, trade_num)
-
-        elif action == "Short" and not self.have_long_position:
-            trade_num = -trade_num
-            self.short_position += trade_num
+        # 开仓 short
+        elif self.short_condition:
+            self.short_position += abs(trade_num)  # self.short_position不带正负号, trade_num带正负号
             new_df, reward = self.update_new_df(action, new_df, commission, trade_num)
-
-        elif action == "Cover" and self.have_short_position:
-            trade_num, done = (trade_num, False) if -trade_num > self.short_position else (-self.short_position, True)
-            self.short_position += trade_num
+        # 平仓 cover
+        elif self.cover_condition:
+            trade_num, done = (trade_num, False) if trade_num < self.short_position else (self.short_position, True)
+            self.short_position -= trade_num  # 注意这里是减持
             new_df, reward = self.update_new_df(action, new_df, commission, trade_num)
 
         # skip 或者 手有多仓 还收到 buy action
@@ -211,14 +224,14 @@ class FutureEnv(gym.Env):
             new_df['hold_money_value'] = self.hold_money_value
 
             new_df['hold_share_value'] = new_df.apply(
-                lambda x: x['<CLOSE>'] * self.contract_prod * self.security_rate * abs(x['net_position']), axis=1)
+                lambda x: x['close_price'] * self.contract_prod * self.security_rate * abs(x['net_position']), axis=1)
+
             if self.have_long_position:
                 new_df['balance'] = new_df['hold_share_value'] + self.hold_money_value
                 new_df['profit'] = new_df['balance'] - self.start_balance
             elif self.have_short_position:
                 new_df['balance'] = 2 * self.start_balance - abs(new_df['hold_share_value']) - self.hold_money_value
                 new_df['profit'] = new_df['balance'] - self.start_balance
-
 
         obs = obs.append(new_df)
         self.cur_state_df = obs
@@ -229,12 +242,12 @@ class FutureEnv(gym.Env):
 
         return obs, reward, done, info
 
-    def update_new_df_position_info(self, trade_num:int, new_df:pd.DataFrame):
+    def update_new_df_position_info(self, trade_num: int, new_df: pd.DataFrame):
 
         new_df['trade_num'] = trade_num
         new_df['long_position'] = self.long_position
         new_df['short_position'] = self.short_position
-        self.net_position = self.long_position + self.short_position
+        self.net_position = self.long_position - self.short_position
         new_df['net_position'] = self.net_position
 
         return new_df
@@ -245,44 +258,34 @@ class FutureEnv(gym.Env):
     def close(self):
         pass
 
-    def action_space_sample(self):
-        action_key_list = list(self.action_ratio_dict.keys())
-        ix, value = self.action_space.sample()
-        return (action_key_list[ix], value)
-
 
 if __name__ == "__main__":
-    # 这里和FutureEnv中声明的类型和结构保持一致
-    # TODO 做测试先设置为0.2, 以后由神经网络给出
-    action_ratio_dict = {
-        "Buy": np.array([0.3], dtype=np.float32),
-        "Sell": np.array([0.3], dtype=np.float32),
-        "Short": np.array([0.4], dtype=np.float32),
-        "Cover": np.array([0.3], dtype=np.float32),
-    }
+    df = pd.read_csv("./data/RBL8.csv")
+    columns_list = [
+        'open_price', 'high_price', 'low_price', 'close_price',
+        'open_interest', 'volume'
+    ]
+    df = df[columns_list]
 
-    df = pd.read_csv("./data/YNDX_150101_151231.csv")
-    env = FutureEnv(prices_df=df.iloc[:, 1:], action_ratio_dict=action_ratio_dict)
-
-    action, value = env.action_space_sample()
-    # Tuple的第一个为action, 后面为对应的数值
-    # (0, array([0.6831512], dtype=float32))
+    env = FutureEnv(prices_df=df)
+    action = env.action_space.sample()
     initial_state_df = env.reset()
 
     # 将起始状态送入model, 给出output action
     reward_list = []
     done_list = []
 
-    obs0, reward, done, info = env.step('Buy')
+    obs0, reward, done, info = env.step(action)
     reward_list.append(reward)
     done_list.append(done)
-    for _ in range(10):
+    for _ in range(100):
         # action_key = random.choice(list(action_ratio_dict.keys()))
         # action = action_ratio_dict[action_key]
 
-        action, value = env.action_space_sample()
+        action = env.action_space.sample()
+        print(action)
         obs, reward, done, info = env.step(action)
-        obs0 = obs0.append(obs)
+        obs0 = obs0.append(obs.iloc[[-1]])
         reward_list.append(reward)
         done_list.append(done)
 
---
+++ vnpy/app/cta_backtester_jnpy/DRL/model/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Datetime :   2020/2/7 下午11:32
+@Author   :   Fangyang
+"""
+
+if __name__ == "__main__":
+    pass
