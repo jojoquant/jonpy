dbbcd75dbec7b5452aec30225fb95db4a72930a9
fangyang_gpu <fangyang.jing@hotmail.com>
2021-12-07 22:35:47 +0800

    [Add] easytrader server

---
+++ jnpy/easytrader_server_run.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Datetime : 2021/12/7 3:56
+# @Author   : Fangyang
+# @Software : PyCharm
+
+from easytrader import server
+
+
+if __name__ == '__main__':
+    server.run()
